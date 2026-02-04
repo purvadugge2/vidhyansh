a06e04de269628620f79a78b75fdf195d33bf1c2
purvadugge2 <purvadugge2@gmail.com>
2026-02-04 18:42:39 +0530

    Add health API and test with Postman

---
+++ app.js
@@ -0,0 +1,1 @@
+console.log("Welcome to Vidhyansh backend");
--- backend/routes/healthRoutes.js
+++ backend/routes/healthRoutes.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const { healthCheck } = require("../controllers/healthController");
 
 router.get("/health", healthCheck);
+router.get("/hello", (req, res) => {
+  res.json({ message: "Hello from Vidhyansh backend" });
+});
+
 
 module.exports = router;
